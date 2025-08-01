7434fc2e6c2a11a14d582e5102670e5abc5a14ed
asierrodri <81629553+asierrodri@users.noreply.github.com>
2025-08-01 12:26:52 +0200

    añadido pruebas control-mesa

--- services/mesaOSC.js
+++ services/mesaOSC.js
@@ -13,6 +13,19 @@ function enviarOSC(ruta, valor) {
   let tipo = typeof valor === 'number' ? 'f' : 's';
   let val = valor;
 
+  udpPort.on("ready", () => {
+    // /save "scene" 83 "Nueva83" ""
+    udpPort.send({
+      address: "/save",
+      args: [
+        { type: "s", value: "scene" },
+        { type: "i", value: 83 },
+        { type: "s", value: "Nueva83" },
+        { type: "s", value: "" }
+      ]
+    });
+  });
+
   // Para /-ssave o /-sload, asegúrate de que el valor sea un string entre comillas
   if ((ruta === '/-ssave' || ruta === '/-sload') && typeof valor === 'string') {
     if (!valor.startsWith('"')) {
